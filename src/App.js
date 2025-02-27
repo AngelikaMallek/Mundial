3b541ae8f3603ae58f0b6a7dad0e4142f899a393
Angelika Krefta <krefta.angelika@gmail.com>
2025-02-27 17:23:30 +0100

    tested used API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import TableTile from './common/TableTile';
+import { useApi } from './API/useAPI';
+import { Container } from './common/Container';
 
 function App() {
-  
+
+    const { teams } = useApi();
+
+    return (
+        <Container >
+            {teams ? teams.map((team) => (
+                <TableTile
+                    test={team.druzyna}
+                />
+            )) : ""}
+        </Container>
+    )
+    
 }
 
 export default App;
---
+++ src/common/Container/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 90vw;
+    max-width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`;
