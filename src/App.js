a2285e8f247dbd8ea707b903e81f9803ae6a6463
Angelika Krefta <krefta.angelika@gmail.com>
2025-03-01 15:24:41 +0100

    changed App.js of return

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import TableTile from './common/TableTile';
-import { useApi } from './API/useAPI';
-import { Container } from './common/Container';
+import TeamsTable from './features/TeamsTable';
 
 function App() {
-
-    const { teams } = useApi();
-
-    return (
-        <Container >
-            {teams ? teams.map((team) => (
-                <TableTile
-                    test={team.druzyna}
-                />
-            )) : ""}
-        </Container>
+    return(
+        <TeamsTable />
     )
-    
 }
 
 export default App;
--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -5,6 +5,6 @@ export const Container = styled.div`
     max-width: 90%;
     margin: 0 auto;
     display: flex;
-    flex-direction: column;
+    flex-wrap: wrap;
     gap: 24px;
 `;
