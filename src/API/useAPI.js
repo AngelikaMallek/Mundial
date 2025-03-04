3b2cd727d456d8cc2acf26f19b259266b2cac017
Angelika Krefta <krefta.angelika@gmail.com>
2025-03-04 16:17:37 +0100

    changed API key

--- src/API/useAPI.js
+++ src/API/useAPI.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 export const useApi = () => {
 
     const supabaseUrl = 'https://ewrwniaqrrxkcmizibyu.supabase.co'
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3cnduaWFxcnJ4a2NtaXppYnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMTM3NTgsImV4cCI6MjA1NjU4OTc1OH0.8JscCEaTrMU0BtclcnFN_LMOdPYCcRZDC67j99HieM8'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3cnduaWFxcnJ4a2NtaXppYnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMDA3OTEsImV4cCI6MjA1NjY3Njc5MX0.kFAl3-lnMEI8DmoPnEWlMiMYKRTprn4fCcSueppE8Go'
     const supabase = createClient(supabaseUrl, supabaseKey)
 
 	const [teams, setTeams] = useState(null);
