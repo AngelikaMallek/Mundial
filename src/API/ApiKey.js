8f673d4359b15262f054e179356cf78ad70d3a7c
Angelika Krefta <krefta.angelika@gmail.com>
2025-03-17 17:38:05 +0100

    added global file to Api key

---
+++ src/API/ApiKey.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+    const supabaseUrl = 'https://ewrwniaqrrxkcmizibyu.supabase.co'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3cnduaWFxcnJ4a2NtaXppYnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMDA3OTEsImV4cCI6MjA1NjY3Njc5MX0.kFAl3-lnMEI8DmoPnEWlMiMYKRTprn4fCcSueppE8Go'
+    const supabase = createClient(supabaseUrl, supabaseKey)
+
+    export default supabase;
