f8ff92c02d430938256807c65501faeb6f9b6d80
Angelika Krefta <krefta.angelika@gmail.com>
2025-04-01 18:26:21 +0200

    added loading and error to CountryTile

--- src/API/useCountryApi.js
+++ src/API/useCountryApi.js
@@ -4,20 +4,22 @@ import { countryApiUrl } from "./ApiKey";
 
 export const useCountryApi = (countryId) => {
 	const [ flags, setFlags ] = useState(null);
+    const [ loading, setLoadnig ] = useState(true);
+    const [ error, setError ] = useState(false);
 
 	useEffect(() => {
         const fetchCountry = async () => {
             try {
                 const response = await axios.get(countryApiUrl)
                 setFlags(response.data)
+                setLoadnig(false)
             } catch {
-                
+                setError(true)
             }
     }
 
-        setTimeout(fetchCountry, 2);
+        setTimeout(fetchCountry, 500);
     },[countryId]);
 
-
-	return { flags };
+	return { flags, loading, error };
 };
