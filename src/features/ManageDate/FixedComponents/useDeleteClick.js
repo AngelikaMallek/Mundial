f8fa3a318de72f77cb5e8cf8e4c6720cf8c79fba
Angelika Krefta <krefta.angelika@gmail.com>
2025-04-07 15:39:35 +0200

    "Improved country deletion"

--- src/features/ManageDate/FixedComponents/useDeleteClick.js
+++ src/features/ManageDate/FixedComponents/useDeleteClick.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FixedWrapper, ToggleFixedButton, Parahraph, List, ListInternalItem } from "../../../common/FixedComponents";
-import { deleteItem } from "./useDeleteItem";
+import { useDeletedItem } from "./useDeleteItem";
 
 export const useDeleteClick = () => {
     const [showConfirm, setShowConfirm] = useState(false);
@@ -24,14 +24,20 @@ export const useDeleteClick = () => {
     }
 }
 
-export const FixedComponentDeleted = ({id, name, setShowConfirm}) => (
-    <FixedWrapper>
-        <Parahraph>Czy napewno chcesz usunąć kraj:<br/> {name}?</Parahraph>
-        <List>
-            <ListInternalItem>
-                <ToggleFixedButton onClick={() => deleteItem(id)}>Tak</ToggleFixedButton>
-                <ToggleFixedButton onClick={() => setShowConfirm(false)}>Nie</ToggleFixedButton>
-            </ListInternalItem>
-        </List>
-    </FixedWrapper>
-)
+export const FixedComponentDeleted = ({id, name, setShowConfirm}) => {
+ 
+    const {deleteItem, validationError} = useDeletedItem();
+
+    return (
+        <FixedWrapper>
+            <Parahraph>Czy napewno chcesz usunąć kraj:<br/> {name}?</Parahraph>
+            <List>
+                <ListInternalItem>
+                    <ToggleFixedButton onClick={() => deleteItem(id)}>Tak</ToggleFixedButton>
+                    <ToggleFixedButton onClick={() => setShowConfirm(false)}>Nie</ToggleFixedButton>
+                </ListInternalItem>
+            </List>
+            <Parahraph>{validationError}</Parahraph>
+        </FixedWrapper>
+    )
+}
--- src/features/ManageDate/FixedComponents/useDeleteItem.js
+++ src/features/ManageDate/FixedComponents/useDeleteItem.js
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import { supabase } from '../../../API/ApiKey';
 
-export const deleteItem = async (itemId) => {
 
-    try {
-        const { error } = await supabase
-            .from('teams')
-            .delete()
-            .eq('id', itemId);
-            
-            window.location.reload();
+export const useDeletedItem = () => {
 
-            if (error) throw error;
-        
-    } catch (error) {
-        console.error('Error in delete process:', error);
+    const [ validationError, setValidationError ] = useState("");
+
+    const deleteItem = async (itemId) => {
+
+        try {
+            const { error } = await supabase
+                .from('teams')
+                .delete()
+                .eq('id', itemId);
+
+                if (error) {
+                    setValidationError("Nie można usunąć drużyny, która już posiada rozegrane mecze!")
+                } else {
+                    window.location.reload();
+                }
+                
+        } catch (error) {
+            console.error('Error in delete process:', error);
+        }
     }
+
+    return { deleteItem, validationError }
 }
