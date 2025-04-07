d7f43b37f279801aa669f011ed3e581559efec54
Angelika Krefta <krefta.angelika@gmail.com>
2025-04-07 16:33:34 +0200

    Updates

--- static/js/952.63f63170.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmundial=self.webpackChunkmundial||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.63f63170.chunk.js.map
